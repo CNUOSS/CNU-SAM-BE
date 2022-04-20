9ac629297d4157799e8a753761112020b1eae484
MoonDD99 <mjh991016@naver.com>
2022-04-20 19:39:45 +0900

    [chore]Init setting

---
+++ src/main/java/gp/cnusambe/CnuSamBeApplication.java
@@ -0,0 +1,13 @@
+package gp.cnusambe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnuSamBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnuSamBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gp/cnusambe/CnuSamBeApplicationTests.java
@@ -0,0 +1,13 @@
+package gp.cnusambe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnuSamBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
