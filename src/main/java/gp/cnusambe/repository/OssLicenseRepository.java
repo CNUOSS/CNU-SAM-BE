7bc4cf0d31577f1a1734886e75111dffdf7e0dc5
MoonDD99 <mjh991016@naver.com>
2022-05-03 11:45:31 +0900

    [feat] Implement creating ossLicense in ossLicenseService
    1. OssLicenseType cascade 오류 수정
    2. OssLicenseService에서 ossLicense 생성하는 함수인 create 구현

--- src/main/java/gp/cnusambe/repository/OssLicenseRepository.java
+++ src/main/java/gp/cnusambe/repository/OssLicenseRepository.java
@@ -2,6 +2,8 @@ package gp.cnusambe.repository;
 
 import gp.cnusambe.domain.OssLicense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface OssLicenseRepository extends JpaRepository<OssLicense,Long> {
+
 }
