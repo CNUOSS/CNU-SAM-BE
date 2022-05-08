82d04a3a24fab7792d2dc28d715ab989df3e9fef
Fortune00 <sinyoungbok99@gmail.com>
2022-05-08 18:37:41 +0900

    [feat][#3] Implement Login in UserController
    - 사용자가 입력한 ID와 pwd로 인증 후, 로그인 응답으로 userId, AccessToken, uuid를 보내준다
    - AccessToken은 만료 전까지 요청 Header의 Authorization에 넣어준다
    - uuid는 Cache에 저장되어 있는 RefreshToken의 Key값이다

--- src/main/java/gp/cnusambe/controller/user/UserController.java
+++ src/main/java/gp/cnusambe/controller/user/UserController.java
@@ -1,10 +1,25 @@
 package gp.cnusambe.controller.user;
 
 import gp.cnusambe.domain.user.User;
+import gp.cnusambe.error.InvalidPasswordException;
+import gp.cnusambe.error.RefreshTokenException;
+import gp.cnusambe.payload.request.LoginRequest;
+import gp.cnusambe.payload.request.LogoutOrRefreshRequest;
 import gp.cnusambe.payload.request.SignupRequest;
+import gp.cnusambe.payload.response.LoginResponse;
+import gp.cnusambe.security.jwt.JwtTokenProvider;
+import gp.cnusambe.security.jwt.UserDetailsImpl;
+import gp.cnusambe.service.user.UserDetailsServiceImpl;
 import gp.cnusambe.service.user.UserService;
+import gp.cnusambe.util.RedisUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +29,11 @@ import java.net.URI;
 @RequiredArgsConstructor
 @RestController
 public class UserController {
+    private final AuthenticationManager authenticationManager;
     private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserDetailsServiceImpl userDetailsServiceImp;
+    private final RedisUtil redisUtil;
 
     @PostMapping("/join")
     public ResponseEntity<String> signUp(@RequestBody SignupRequest signUpRequest) throws Exception {
@@ -26,5 +45,34 @@ public class UserController {
                 .fromCurrentContextPath().path("/users/")
                 .buildAndExpand(user.getUserId()).toUri();
         return ResponseEntity.created(location).body(user.getUserId());
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest){
+        Authentication authentication;
+        try {
+            authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginRequest.getUserId(), loginRequest.getPassword()));
+        } catch (BadCredentialsException e) {
+            throw new InvalidPasswordException();
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserDetailsImpl userDetailsImpl = (UserDetailsImpl)authentication.getPrincipal();
+        LoginResponse jwtResponse = generateAndSaveToken(userDetailsImpl);
+
+        return new ResponseEntity<>(jwtResponse, HttpStatus.OK);
+    }
+
+    private LoginResponse generateAndSaveToken(UserDetailsImpl userDetailsImpl) {
+        String userId = userDetailsImpl.getUserId();
+        String uuid = UUID.randomUUID().toString();
+        String accessToken = jwtTokenProvider.generateJwtToken(userDetailsImpl);
+        String refreshToken = jwtTokenProvider.generateRefreshToken(userDetailsImpl);
+
+        redisUtil.setDataExpire(uuid, refreshToken, (int) JwtTokenProvider.REFRESH_EXPIRATION_SECONDS);
+
+        return new LoginResponse(userId, accessToken, uuid);
     }
 }
