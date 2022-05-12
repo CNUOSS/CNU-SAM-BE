6a1cc8e26db3c3e1fb9b6cf4b413be92ad9e507e
MoonDD99 <mjh991016@naver.com>
2022-05-13 01:35:25 +0900

    [feat][#13] Implement Searching OssLicenseList
    1. 검색값 구분해서 OssLicenseList 조회 구현

--- src/main/java/gp/cnusambe/repository/RestrictionRepository.java
+++ src/main/java/gp/cnusambe/repository/RestrictionRepository.java
@@ -3,5 +3,8 @@ package gp.cnusambe.repository;
 import gp.cnusambe.domain.Restriction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RestrictionRepository extends JpaRepository<Restriction,String> {
+    List<Restriction> findByRestrictionNameContaining(String keyword);
 }
