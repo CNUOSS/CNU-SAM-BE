c7f12c57e13a360ea0e22c4fcf0d8afe4a30f1d3
MoonDD99 <mjh991016@naver.com>
2022-05-13 19:37:58 +0900

    [feat][#13] Implement response with meta data when get osslicenselist
    1. OssLicenseList 조회할때 응답에 metat data도 함께 보내는 것 구현
    2. Pageable하게 코드 리팩토링
    3. Message Class 삭제

---
+++ src/main/java/gp/cnusambe/payload/response/OssLicenseListResponse.java
@@ -0,0 +1,24 @@
+package gp.cnusambe.payload.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import gp.cnusambe.dto.OssLicenseDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class OssLicenseListResponse {
+    private MetaResponse meta;
+    private List<OssLicenseDto> OssLicense;
+
+
+    public OssLicenseListResponse makeResponse(Page<OssLicenseDto> licenseDtoPage){
+        return new OssLicenseListResponse(new MetaResponse(licenseDtoPage.getTotalElements(),false),licenseDtoPage.getContent());
+    }
+}
