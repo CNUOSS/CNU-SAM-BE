7e2c3105f52840a85d32303917a3dfd5b22ddef6
MoonDD99 <mjh991016@naver.com>
2022-05-17 16:42:07 +0900

    [feat][#30] Implement Get RestrictionNameData
    1. RestrictionName 데이터 가져오는 API 구현

--- src/main/java/gp/cnusambe/repository/RestrictionRepository.java
+++ src/main/java/gp/cnusambe/repository/RestrictionRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface RestrictionRepository extends JpaRepository<Restriction,String> {
-    List<Restriction> findByRestrictionNameContaining(String keyword);
+    List<Restriction> findAll();
 }
