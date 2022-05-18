b93c463d3819c44e7dfe512f728322c4d62bde7d
MoonDD99 <mjh991016@naver.com>
2022-05-19 00:12:43 +0900

    [feat][#35] Implement Creating Project in ProjectController
    1. ProjectConroller에서 project 생성하는 기능 구현
    2. ProjectDto에서 ProjectCategoryName 타입 변경

---
+++ src/main/java/gp/cnusambe/repository/ProjectCategoryRepository.java
@@ -0,0 +1,7 @@
+package gp.cnusambe.repository;
+
+import gp.cnusambe.domain.ProjectCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectCategoryRepository extends JpaRepository<ProjectCategory,String> {
+}
--- src/main/java/gp/cnusambe/service/ProjectService.java
+++ src/main/java/gp/cnusambe/service/ProjectService.java
@@ -2,15 +2,18 @@ package gp.cnusambe.service;
 
 import gp.cnusambe.domain.OssLicense;
 import gp.cnusambe.domain.Project;
+import gp.cnusambe.domain.ProjectCategory;
 import gp.cnusambe.domain.User;
 import gp.cnusambe.dto.ProjectDto;
 import gp.cnusambe.repository.OssLicenseRepository;
+import gp.cnusambe.repository.ProjectCategoryRepository;
 import gp.cnusambe.repository.ProjectRepository;
 import gp.cnusambe.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -18,13 +21,15 @@ import java.util.Optional;
 public class ProjectService {
     private final ProjectRepository projectRepository;
     private final OssLicenseRepository ossLicenseRepository;
+    private final ProjectCategoryRepository projectCategoryRepository;
     private final UserRepository userRepository;
 
     public ProjectDto create(ProjectDto projectDto){
         //Dto -> Entity
         Optional<OssLicense> license = this.ossLicenseRepository.findById(projectDto.getOssLicenseId());
         Optional<User> user = this.userRepository.findByUserId(projectDto.getUserId());
-        Project project = projectDto.makeProjectEntity(license.get(),user.get());
+        Optional<ProjectCategory> projectCategory = this.projectCategoryRepository.findById(projectDto.getProjectCategoryName());
+        Project project = projectDto.makeProjectEntity(license.get(),user.get(),projectCategory.get());
 
         //Save Project
         Project newProject = this.projectRepository.save(project);
