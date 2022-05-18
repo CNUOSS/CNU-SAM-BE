954420fe234d6dd33077d96e60bdd118da3962e7
Fortune00 <sinyoungbok99@gmail.com>
2022-05-18 23:43:11 +0900

    [feat] Implement LectureType & LectureTypeRepository

---
+++ src/main/java/gp/cnusambe/repository/LectureTypeRepository.java
@@ -0,0 +1,9 @@
+package gp.cnusambe.repository;
+
+import gp.cnusambe.domain.LectureType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface LectureTypeRepository extends JpaRepository<LectureType, String> {
+    List<LectureType> findAll();
+}
