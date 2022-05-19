1f6f241f0c06536ab128972c5759735e1543d41a
Fortune00 <sinyoungbok99@gmail.com>
2022-05-19 13:02:22 +0900

    [feat] Implement getAllDepartments in SWService

--- src/main/java/gp/cnusambe/service/SWService.java
+++ src/main/java/gp/cnusambe/service/SWService.java
@@ -1,17 +1,25 @@
 package gp.cnusambe.service;
 
+import gp.cnusambe.domain.Department;
 import gp.cnusambe.domain.LectureType;
+import gp.cnusambe.repository.DepartmentRepository;
 import gp.cnusambe.repository.LectureTypeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SWService {
     private final LectureTypeRepository lectureTypeRepository;
+    private final DepartmentRepository departmentRepository;
 
-    public List<LectureType> getAllLectureTypes(){
+    public List<LectureType> getAllLectureTypes() {
         return lectureTypeRepository.findAll();
+    }
+
+    public List<Department> getAllDepartments(){
+        return departmentRepository.findAll();
     }
 }
