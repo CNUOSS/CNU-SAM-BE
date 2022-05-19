304b05c5c371340988f758f397509b4648007fe1
Fortune00 <sinyoungbok99@gmail.com>
2022-05-19 16:26:03 +0900

    [feat] Implement SubscriptionSW & SubscriptionSWRepository

---
+++ src/main/java/gp/cnusambe/repository/SubscriptionSWRepository.java
@@ -0,0 +1,7 @@
+package gp.cnusambe.repository;
+
+import gp.cnusambe.domain.SubscriptionSW;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscriptionSWRepository extends JpaRepository<SubscriptionSW, Long> {
+}
