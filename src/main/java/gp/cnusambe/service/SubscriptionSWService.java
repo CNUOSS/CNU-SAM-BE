e9192655a9e6fb5cad5667fcc7fa805baeb049bd
Fortune00 <sinyoungbok99@gmail.com>
2022-05-22 13:47:39 +0900

    [feat] Implement readAllSubscriptionSW in SubscriptionSWService

--- src/main/java/gp/cnusambe/service/SubscriptionSWService.java
+++ src/main/java/gp/cnusambe/service/SubscriptionSWService.java
@@ -3,21 +3,36 @@ package gp.cnusambe.service;
 import gp.cnusambe.domain.SubscriptionSW;
 import gp.cnusambe.exception.custom.SWNotFoundException;
 import gp.cnusambe.payload.request.SubscriptionSWRequest;
+import gp.cnusambe.payload.response.SubscriptionSWResponse;
 import gp.cnusambe.repository.SubscriptionSWRepository;
+import gp.cnusambe.repository.SwSubscriptionQueryRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SubscriptionSWService {
+    private final ModelMapper modelMapper;
     private final SubscriptionSWRepository subscriptionSWRepository;
+    private final SwSubscriptionQueryRepository swSubscriptionQueryRepository;
 
-    public SubscriptionSW createSubscriptionSW(SubscriptionSWRequest request){
-        SubscriptionSW sw = new SubscriptionSW(request);
-        return subscriptionSWRepository.save(sw);
+    public SubscriptionSWResponse createSubscriptionSW(SubscriptionSWRequest request) {
+        SubscriptionSW sw = subscriptionSWRepository.save(new SubscriptionSW(request));
+        return modelMapper.map(sw, SubscriptionSWResponse.class);
     }
 
-    public void deleteSubscriptionSW(Long swId){
+    public Page<SubscriptionSWResponse> readAllSubscriptionSW
+            (boolean search, String swType, String swManufacturer, String swName, Pageable pageable) {
+        Page<SubscriptionSW> sw = search
+                ? swSubscriptionQueryRepository.findAllBy(swType, swManufacturer, swName, pageable)
+                : subscriptionSWRepository.findAll(pageable);
+        return sw.map(element -> modelMapper.map(element, SubscriptionSWResponse.class));
+    }
+
+    public void deleteSubscriptionSW(Long swId) {
         SubscriptionSW sw = subscriptionSWRepository.findById(swId).orElseThrow(SWNotFoundException::new);
         subscriptionSWRepository.delete(sw);
     }
