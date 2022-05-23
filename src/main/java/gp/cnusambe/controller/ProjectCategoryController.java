9bd6122c1e8797d3bcd29b941eece5d842c863fb
MoonDD <mjh991016@naver.com>
2022-05-23 19:42:18 +0900

    [feat][#29] Implement Get ProjectCategoryName Data
    1. ProjectCategoryName 데이터 조회기능 구현

---
+++ src/main/java/gp/cnusambe/controller/ProjectCategoryController.java
@@ -0,0 +1,23 @@
+package gp.cnusambe.controller;
+
+import gp.cnusambe.domain.ProjectCategory;
+import gp.cnusambe.service.ProjectCategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class ProjectCategoryController {
+    private final ProjectCategoryService projectCategoryService;
+
+    @GetMapping("/category-names")
+    public ResponseEntity<List<ProjectCategory>> getCategoryNameData(){
+        List<ProjectCategory> categoryList = this.projectCategoryService.getProjectCategory();
+        return new ResponseEntity<>(categoryList, HttpStatus.OK);
+    }
+}
