4d6029e1e13e421a02f183b7c89f85f6232a03f7
Fortune00 <sinyoungbok99@gmail.com>
2022-05-26 22:48:27 +0900

    [feat] Implement DTOs of RegistrationSW for Post

---
+++ src/main/java/gp/cnusambe/payload/request/RegistrationSWRequest.java
@@ -0,0 +1,18 @@
+package gp.cnusambe.payload.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class RegistrationSWRequest {
+    private String latestUpdaterId;
+    private String swManufacturer;
+    private String swName;
+    private boolean isManaged;
+}
