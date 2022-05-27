32c77c544fea3f8d807851075de9472ccdde6b17
Fortune00 <sinyoungbok99@gmail.com>
2022-05-27 19:18:23 +0900

    [feat] Implement getAllRegistrationsW in RegisterationSWController

--- src/main/java/gp/cnusambe/controller/RegistrationSWController.java
+++ src/main/java/gp/cnusambe/controller/RegistrationSWController.java
@@ -1,14 +1,23 @@
 package gp.cnusambe.controller;
 
+import gp.cnusambe.dto.PageInfoDto;
+import gp.cnusambe.dto.RegistrationSWDto;
 import gp.cnusambe.payload.request.RegistrationSWRequest;
 import gp.cnusambe.payload.response.ManufacturerResponse;
+import gp.cnusambe.payload.response.RegistrationSWListResponse;
 import gp.cnusambe.payload.response.RegistrationSWResponse;
 import gp.cnusambe.service.RegistrationSWService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,6 +27,21 @@ public class RegistrationSWController {
     @PostMapping("/registrations")
     public ResponseEntity<RegistrationSWResponse> postRegistrationSW(@RequestBody RegistrationSWRequest request){
         RegistrationSWResponse response = new RegistrationSWResponse(registerSWService.createRegistrationSW(request));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/registrations/search")
+    public ResponseEntity<RegistrationSWListResponse> getAllRegistrationsW(
+            @RequestParam(value="sw-mfr", required=false) String swManufacturer,
+            @RequestParam(value="sw-name", required=false) String swName,
+            @PageableDefault(size=9, page=0, sort="latestUpdateDate", direction= Sort.Direction.DESC) Pageable pageable
+    ){
+        String swManufacturer_ = Optional.ofNullable(swManufacturer).orElse("");
+        String swName_ = Optional.ofNullable(swName).orElse("");
+
+        Page<RegistrationSWDto> pageOfSW = registerSWService.readAllRegistrationSW(swManufacturer_, swName_, pageable);
+        PageInfoDto pageInfo = new PageInfoDto(pageOfSW.getTotalElements(), pageOfSW.isLast(), pageOfSW.getTotalPages(), pageOfSW.getSize());
+        RegistrationSWListResponse response = new RegistrationSWListResponse(pageInfo, pageOfSW.stream().collect(Collectors.toList()));
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
