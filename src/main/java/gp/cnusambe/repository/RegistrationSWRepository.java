40c56ce9ef86d00000769683a4e9253da48e4220
Fortune00 <sinyoungbok99@gmail.com>
2022-05-27 19:16:45 +0900

    [feat] Implement & Apply RegistrationSWQueryRepository

--- src/main/java/gp/cnusambe/repository/RegistrationSWRepository.java
+++ src/main/java/gp/cnusambe/repository/RegistrationSWRepository.java
@@ -1,9 +1,15 @@
 package gp.cnusambe.repository;
 
 import gp.cnusambe.domain.RegistrationSW;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import java.util.Optional;
 
-public interface RegistrationSWRepository extends JpaRepository<RegistrationSW, Long>{
+public interface RegistrationSWRepository extends JpaRepository<RegistrationSW, Long>, JpaSpecificationExecutor<RegistrationSW> {
     Optional<RegistrationSW> findById(Long swId);
+    Page<RegistrationSW> findAllByIsManaged(boolean isManaged, Pageable pageable);
+    Page<RegistrationSW> findAll(Specification spec, Pageable pageable);
 }
