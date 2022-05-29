09df64dd38bc417d5c36e147400b06a8c4df1fb8
Fortune00 <sinyoungbok99@gmail.com>
2022-05-29 20:22:07 +0900

    [feat] Implement Dto & Payload for LectureSW

---
+++ src/main/java/gp/cnusambe/payload/response/LectureSWResponse.java
@@ -0,0 +1,19 @@
+package gp.cnusambe.payload.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import gp.cnusambe.dto.LectureMapDto;
+import gp.cnusambe.dto.LectureSWDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class LectureSWResponse {
+    private LectureSWDto lectureSW;
+    private List<LectureMapDto> sw;
+}
