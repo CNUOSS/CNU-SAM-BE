dff833fa6bee1949b144e0077d73639fabb571e7
Fortune00 <sinyoungbok99@gmail.com>
2022-05-29 20:28:26 +0900

    [feat] Implement getLectureSW in LectureSWController
    - lectureSW는 수업의 정보, lectureMap은 등록된 sw에 대한 정보를 다룸
    - lectureSW와 lectureMap을 같이 Response에 반환

--- src/main/java/gp/cnusambe/controller/LectureSWController.java
+++ src/main/java/gp/cnusambe/controller/LectureSWController.java
@@ -1,26 +1,51 @@
 package gp.cnusambe.controller;
 
+import gp.cnusambe.dto.LectureMapDto;
+import gp.cnusambe.dto.LectureSWDto;
+import gp.cnusambe.dto.SWInLectureSWDto;
+import gp.cnusambe.payload.request.LectureSWRequest;
 import gp.cnusambe.payload.response.DepartmentResponse;
+import gp.cnusambe.payload.response.LectureSWResponse;
 import gp.cnusambe.payload.response.LectureTypeResponse;
 import gp.cnusambe.service.LectureSWService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
 public class LectureSWController {
+    private final ModelMapper modelMapper;
     private final LectureSWService lectureSwService;
 
+    @PostMapping("/lectures")
+    public ResponseEntity<LectureSWResponse> getLectureSW(@RequestBody LectureSWRequest request) {
+        LectureSWDto lectureSwDto = modelMapper.map(request, LectureSWDto.class);
+        lectureSwDto = lectureSwService.createLectureSW(lectureSwDto);
+
+        List<SWInLectureSWDto> swInLectureSWDtos = request.getSw()
+                .stream().map(element -> modelMapper.map(element, SWInLectureSWDto.class))
+                .collect(Collectors.toList());
+        List<LectureMapDto> lectureMapDtos = lectureSwService.createAllLectureMap(lectureSwDto.getId(), swInLectureSWDtos);
+
+        LectureSWResponse response = new LectureSWResponse(lectureSwDto, lectureMapDtos);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
     @GetMapping("/lecture-types")
-    public ResponseEntity<List<LectureTypeResponse>> getLectureTypes(){
+    public ResponseEntity<List<LectureTypeResponse>> getLectureTypes() {
         return new ResponseEntity<>(lectureSwService.getAllLectureTypes(), HttpStatus.OK);
     }
+
     @GetMapping("/departments")
-    public ResponseEntity<List<DepartmentResponse>> getDepartments(){
+    public ResponseEntity<List<DepartmentResponse>> getDepartments() {
         return new ResponseEntity<>(lectureSwService.getAllDepartments(), HttpStatus.OK);
     }
 }
