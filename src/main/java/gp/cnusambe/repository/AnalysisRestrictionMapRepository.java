02cf25d552bbd526af9b5080a1be8b2afcb7d75c
MoonDD <mjh991016@naver.com>
2022-05-29 16:24:46 +0900

    [feat][#19] Implement Delete Version
    1. Version 삭제하기 기능 구현

--- src/main/java/gp/cnusambe/repository/AnalysisRestrictionMapRepository.java
+++ src/main/java/gp/cnusambe/repository/AnalysisRestrictionMapRepository.java
@@ -5,5 +5,8 @@ import gp.cnusambe.domain.OssAnalysisMap;
 import gp.cnusambe.domain.Restriction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AnalysisRestrictionMapRepository extends JpaRepository<AnalysisRestrictionMap, Long> {
+    List<AnalysisRestrictionMap> findAllByAnalysisMap_Id(Long id);
 }
