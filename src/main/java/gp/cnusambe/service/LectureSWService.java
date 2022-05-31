6593dbe239e78d7671a2a8f182ab5026134f20ae
Fortune00 <sinyoungbok99@gmail.com>
2022-06-01 00:36:08 +0900

    [feat] Implemet readAllLectureSW in LectureSWService

--- src/main/java/gp/cnusambe/service/LectureSWService.java
+++ src/main/java/gp/cnusambe/service/LectureSWService.java
@@ -1,21 +1,17 @@
 package gp.cnusambe.service;
 
-import gp.cnusambe.domain.LectureMap;
-import gp.cnusambe.domain.LectureSW;
-import gp.cnusambe.domain.RegistrationSW;
-import gp.cnusambe.domain.SWInLectureSW;
-import gp.cnusambe.dto.LectureMapDto;
-import gp.cnusambe.dto.LectureSWDto;
-import gp.cnusambe.dto.SWInLectureSWDto;
+import gp.cnusambe.domain.*;
+import gp.cnusambe.dto.*;
 import gp.cnusambe.exception.custom.SWNotFoundException;
 import gp.cnusambe.payload.response.DepartmentResponse;
 import gp.cnusambe.payload.response.LectureTypeResponse;
 import gp.cnusambe.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -24,12 +20,13 @@ public class LectureSWService {
     private final ModelMapper modelMapper;
     private final LectureTypeRepository lectureTypeRepository;
     private final DepartmentRepository departmentRepository;
-    private final LectureSWRepository lectureRepository;
+    private final LectureSWRepository lectureSWRepository;
     private final LectureMapRepository lectureMapRepository;
     private final RegistrationSWRepository registrationSWRepository;
+    private final LectureSWQueryRepository lectureSWQueryRepository;
 
     public LectureSWDto createLectureSW(LectureSWDto swDto) {
-        LectureSW sw = lectureRepository.save(new LectureSW(swDto));
+        LectureSW sw = lectureSWRepository.save(new LectureSW(swDto));
         return modelMapper.map(sw, LectureSWDto.class);
     }
 
@@ -46,26 +43,49 @@ public class LectureSWService {
         return listOfLectureMapDto;
     }
 
-    private LectureMap createLectureMap(Long lectureSWId, Long registrationSWId) {
-        return lectureMapRepository.save(new LectureMap(lectureSWId, registrationSWId));
+    private LectureMap createLectureMap(Long lectureSWId, Long registrationSWId, Date latestUpdateDate) {
+        return lectureMapRepository.save(new LectureMap(lectureSWId, registrationSWId, latestUpdateDate));
     }
 
-    public void deleteLectureSW(Long swId){
+    public LectureSWListDto readAllLectureSW(String department, String year, String lectureType, String semester, String lectureName, String lectureNum, String owner, Pageable pageable) {
+        boolean search = department.length() == 0 && year.length() == 0 && lectureType.length() == 0 && semester.length() == 0 && lectureName.length() == 0 && lectureNum.length() == 0 && owner.length() == 0 ? false : true;
+
+        List<LectureSW> listOfSW = search
+                ? lectureSWQueryRepository.findAllBy(department, year, lectureType, semester, lectureName, lectureNum, owner)
+                : lectureSWRepository.findAll();
+        List<Long> listOfSWId = listOfSW.stream().map(LectureSW::getId).collect(Collectors.toList());
+        Page<LectureMap> pageOfMap = lectureMapRepository.findByLectureSWIdIn(listOfSWId, pageable);
+
+        PageInfoDto pageInfo = new PageInfoDto(pageOfMap.getTotalElements(), pageOfMap.isLast(), pageOfMap.getTotalPages(), pageOfMap.getSize());
+        return new LectureSWListDto(pageInfo, makeListOfLectureSWListDto(pageOfMap.getContent()));
+    }
+
+    private List<LectureSWList> makeListOfLectureSWListDto(List<LectureMap> pageOfMap){
+        List<LectureSWList> listOfLectureSWList = new ArrayList<>();
+        for (LectureMap lectureMap : pageOfMap) {
+            LectureSW lectureSW = lectureSWRepository.findById(lectureMap.getLectureSWId()).get();
+            RegistrationSW registrationSW = registrationSWRepository.findById(lectureMap.getRegistrationSWId()).get();
+            listOfLectureSWList.add(new LectureSWList(lectureSW, registrationSW));
+        }
+        return listOfLectureSWList;
+    }
+
+    public void deleteLectureSW(Long swId) {
         List<LectureMap> lectureMaps = lectureMapRepository.findAllByLectureSWId(swId);
-        for(LectureMap map : lectureMaps)
+        for (LectureMap map : lectureMaps)
             lectureMapRepository.deleteByLectureSWId(map.getLectureSWId());
-        LectureSW sw = lectureRepository.findById(swId).orElseThrow(SWNotFoundException::new);
-        lectureRepository.delete(sw);
+        LectureSW sw = lectureSWRepository.findById(swId).orElseThrow(SWNotFoundException::new);
+        lectureSWRepository.delete(sw);
     }
 
-    public List<LectureTypeResponse> getAllLectureTypes() {
+    public List<LectureTypeResponse> readAllLectureTypes() {
         return lectureTypeRepository.findAll()
                 .stream()
                 .map(element -> modelMapper.map(element, LectureTypeResponse.class))
                 .collect(Collectors.toList());
     }
 
-    public List<DepartmentResponse> getAllDepartments() {
+    public List<DepartmentResponse> readAllDepartments() {
         return departmentRepository.findAll()
                 .stream()
                 .map(element -> modelMapper.map(element, DepartmentResponse.class))
