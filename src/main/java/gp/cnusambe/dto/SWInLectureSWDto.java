427da1c4ab45d16bebe3fe8bee6bc9099e526efb
Fortune00 <sinyoungbok99@gmail.com>
2022-06-02 13:07:46 +0900

    [refactor] Resolve Conflict

--- src/main/java/gp/cnusambe/dto/SWInLectureSWDto.java
+++ src/main/java/gp/cnusambe/dto/SWInLectureSWDto.java
@@ -2,6 +2,7 @@ package gp.cnusambe.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,8 +10,10 @@ import lombok.Setter;
 @Setter
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class SWInLectureSWDto {
+    private Long registrationSWId;
     private String swManufacturer;
     private String swName;
 }
