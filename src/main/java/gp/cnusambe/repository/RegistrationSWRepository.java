2b646f47ee955d93e37652224166311dae5553ac
Fortune00 <sinyoungbok99@gmail.com>
2022-06-03 16:41:05 +0900

    [feat] Implement Repository Function for SWName

--- src/main/java/gp/cnusambe/repository/RegistrationSWRepository.java
+++ src/main/java/gp/cnusambe/repository/RegistrationSWRepository.java
@@ -7,11 +7,13 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import java.util.Optional;
+import java.util.List;
 
 public interface RegistrationSWRepository extends JpaRepository<RegistrationSW, Long>, JpaSpecificationExecutor<RegistrationSW> {
     Optional<RegistrationSW> findById(Long swId);
     Optional<RegistrationSW> findBySwManufacturerAndSwNameAndIsManaged(String swManufacturer, String swName, boolean isManaged);
     Page<RegistrationSW> findAllByIsManaged(boolean isManaged, Pageable pageable);
+    List<RegistrationSW> findAllByIsManaged(boolean isManaged);
     Optional<RegistrationSW> findByIdAndIsManaged(Long swId, boolean isManaged);
     Page<RegistrationSW> findAll(Specification spec, Pageable pageable);
 }
