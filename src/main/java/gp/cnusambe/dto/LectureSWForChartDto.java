976fbde560a430bfbfa0405859dd919ee2aeebf4
Fortune00 <sinyoungbok99@gmail.com>
2022-06-03 15:20:52 +0900

    [feat] Implement Dto & Payload for LectureSWForChart

---
+++ src/main/java/gp/cnusambe/dto/LectureSWForChartDto.java
@@ -0,0 +1,19 @@
+package gp.cnusambe.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import gp.cnusambe.domain.RegistrationSW;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class LectureSWForChartDto {
+    private RegistrationSW registrationSW;
+    private Integer count;
+}
