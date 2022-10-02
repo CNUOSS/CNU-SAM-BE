9130019ccd467e476632b2bc7c0dc1d14c842490
fortune00 <sinyoungbok99@gmail.com>
2022-10-02 15:44:24 +0900

    [refactor] Delete Useless Code

--- src/main/java/gp/cnusambe/repository/SubscriptionSWRepository.java
+++ src/main/java/gp/cnusambe/repository/SubscriptionSWRepository.java
@@ -10,7 +10,6 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import java.util.Optional;
 
 public interface SubscriptionSWRepository extends JpaRepository<SubscriptionSW, Long>, JpaSpecificationExecutor<SubscriptionSW>  {
 
