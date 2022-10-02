612ca898a2b67f2a003c095a39c8791a76f98b8a
fortune00 <sinyoungbok99@gmail.com>
2022-10-02 15:44:24 +0900

    [refactor] Delete Useless Code

--- src/main/java/gp/cnusambe/repository/LectureMapRepository.java
+++ src/main/java/gp/cnusambe/repository/LectureMapRepository.java
@@ -1,8 +1,6 @@
 package gp.cnusambe.repository;
 
 import gp.cnusambe.repository.domain.LectureMap;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.transaction.Transactional;
@@ -10,7 +8,6 @@ import java.util.List;
 
 public interface LectureMapRepository extends JpaRepository<LectureMap, Long> {
     List<LectureMap> findAllByLectureSWId(Long lectureSWId);
-    Page<LectureMap> findAllByLectureSWIdIn(List<Long> lectureSWId, Pageable pageable);
     List<LectureMap> findAllByLectureSWIdIn(List<Long> lectureSWId);
     @Transactional
     void deleteByLectureSWId(Long lectureSWId);
